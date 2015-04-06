78eb0b89a085a3c4a9b959241525963c9754a1d0
shockham <samuel.hockham@gmail.com>
2015-04-06 14:50:59 +0100

    init commit

---
+++ src/main.rs
@@ -0,0 +1,119 @@
+extern crate glutin;
+
+#[macro_use]
+extern crate glium;
+
+use glium::Surface;
+
+mod support;
+
+fn main() {
+    use glium::DisplayBuild;
+
+    // building the display, ie. the main object
+    let display = glutin::WindowBuilder::new()
+        .with_depth_buffer(24)
+        .build_glium()
+        .unwrap();
+
+    // building the vertex and index buffers
+    let vertex_buffer = support::load_wavefront(&display, include_bytes!("support/teapot.obj"));
+
+    // the program
+    let program = match glium::Program::from_source(&display,
+        // vertex shader
+        "
+            #version 330
+            
+            uniform mat4 projection_matrix;
+            uniform mat4 modelview_matrix;
+            
+            in vec3 position;
+            in vec3 normal;
+
+            out vec3 v_normal;
+
+            void main() {
+                v_normal = normal;
+                gl_Position = projection_matrix * modelview_matrix * vec4(position, 1.0);
+            }
+        ",
+
+        // fragment shader
+        "
+            #version 330
+
+            const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+
+            in vec3 v_normal;
+            
+            out vec4 frag_output;
+
+            void main() {
+                float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
+                vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
+                frag_output = vec4(color, 1.0);
+            }
+        ",
+
+        // geometry shader
+        None){
+            Ok(p) => p,
+            Err(e) => panic!("{}", e), 
+        };
+    
+    //quick and dirty vars for cam movement
+    let mut cam_z = 0.0f32;
+    let mut cam_x = 0.0f32;
+    let move_speed = 0.8f32;
+
+    // the main loop
+    support::start_loop(|| {
+        // building the uniforms
+        let uniforms = uniform! {
+           projection_matrix: support::build_persp_proj_mat(60f32, 800f32/600f32, 0.01f32, 1000f32),
+            modelview_matrix: [
+                [1.0f32, 0.0, 0.0, 0.0],
+                [0.0, 1.0f32, 0.0, 0.0],
+                [0.0, 0.0, 1.0f32, 0.0],
+                [cam_x, 0.0, cam_z, 1.0f32]
+            ]
+        };
+
+        // draw parameters
+        let params = glium::DrawParameters {
+            depth_test: glium::DepthTest::IfLess,
+            depth_write: true,
+            .. std::default::Default::default()
+        };
+
+        // drawing a frame
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.0, 0.0, 0.0, 0.0), 1.0);
+        target.draw(&vertex_buffer,
+                    &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
+                    &program, &uniforms, &params).unwrap();
+        target.finish();
+
+        // polling and handling the events received by the window
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::Closed => return support::Action::Stop,
+                glutin::Event::KeyboardInput(glutin::ElementState::Pressed, _, vkey) => {
+                    match vkey{
+                        Some(glutin::VirtualKeyCode::Escape) => return support::Action::Stop,
+                        Some(glutin::VirtualKeyCode::W) => cam_z += move_speed,
+                        Some(glutin::VirtualKeyCode::S) => cam_z -= move_speed,
+                        Some(glutin::VirtualKeyCode::A) => cam_x += move_speed,
+                        Some(glutin::VirtualKeyCode::D) => cam_x -= move_speed,
+                        Some(k) => println!("key: {:?}", k),
+                        _ => ()
+                    }
+                }, 
+                _ => ()
+            }
+        }
+
+        support::Action::Continue
+    });
+}
---
+++ src/support/mod.rs
@@ -0,0 +1,108 @@
+#![allow(dead_code)]
+
+extern crate genmesh;
+extern crate clock_ticks;
+extern crate obj;
+
+use std::thread;
+use glium::{self, Display};
+use glium::vertex::VertexBufferAny;
+
+pub mod camera;
+
+pub enum Action {
+    Stop,
+    Continue,
+}
+
+pub fn start_loop<F>(mut callback: F) where F: FnMut() -> Action {
+    let mut accumulator = 0;
+    let mut previous_clock = clock_ticks::precise_time_ns();
+
+    loop {
+        match callback() {
+            Action::Stop => break,
+            Action::Continue => ()
+        };
+
+        let now = clock_ticks::precise_time_ns();
+        accumulator += now - previous_clock;
+        previous_clock = now;
+
+        const FIXED_TIME_STAMP: u64 = 16666667;
+        while accumulator >= FIXED_TIME_STAMP {
+            accumulator -= FIXED_TIME_STAMP;
+
+            // if you have a game, update the state here
+        }
+
+        thread::sleep_ms(((FIXED_TIME_STAMP - accumulator) / 1000000) as u32);
+    }
+}
+
+/// Returns a vertex buffer that should be rendered as `TrianglesList`.
+pub fn load_wavefront(display: &Display, data: &[u8]) -> VertexBufferAny {
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 3],
+        normal: [f32; 3],
+        texture: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position, normal, texture);
+
+    let mut data = ::std::io::BufReader::new(data);
+    let data = obj::Obj::load(&mut data);
+
+    let mut vertex_data = Vec::new();
+
+    for shape in data.object_iter().next().unwrap().group_iter().flat_map(|g| g.indices().iter()) {
+        match shape {
+            &genmesh::Polygon::PolyTri(genmesh::Triangle { x: v1, y: v2, z: v3 }) => {
+                for v in [v1, v2, v3].iter() {
+                    let position = data.position()[v.0];
+                    let texture = v.1.map(|index| data.texture()[index]);
+                    let normal = v.2.map(|index| data.normal()[index]);
+
+                    let texture = texture.unwrap_or([0.0, 0.0]);
+                    let normal = normal.unwrap_or([0.0, 0.0, 0.0]);
+
+                    vertex_data.push(Vertex {
+                        position: position,
+                        normal: normal,
+                        texture: texture,
+                    })
+                }
+            },
+            _ => unimplemented!()
+        }
+    }
+
+    glium::vertex::VertexBuffer::new(display, vertex_data).into_vertex_buffer_any()
+}
+
+
+pub fn build_persp_proj_mat(fov:f32,aspect:f32,znear:f32,zfar:f32) -> [[f32; 4]; 4] {
+    let ymax = znear * (fov * (3.141592653589793/360.0)).tan();
+    let ymin = -ymax;
+    let xmax = ymax * aspect;
+    let xmin = ymin * aspect;
+
+    let width = xmax - xmin;
+    let height = ymax - ymin;
+
+    let depth = zfar - znear;
+    let q = -(zfar + znear) / depth;
+    let qn = -2.0 * (zfar * znear) / depth;
+
+    let w = 2.0 * znear / width;
+    let h = 2.0 * znear / height;
+    
+    let mut m:[[f32; 4]; 4] = [[0.0f32; 4]; 4];
+    m[0]  = [w, 0.0f32, 0.0f32, 0.0f32];
+    m[1]  = [0.0f32, h, 0.0f32, 0.0f32];
+    m[2]  = [0.0f32, 0.0f32, q, -1.0f32];
+    m[3] = [0.0f32, 0.0f32, qn, 0.0f32];
+
+    return m;
+}
