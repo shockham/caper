bc7e374c4261a7e3e05be509c83a948efe2a778f
shockham <samuel.hockham@gmail.com>
2015-11-23 20:09:31 +0000

    refactored perlin_example a bit

--- examples/perlin_example.rs
+++ examples/perlin_example.rs
@@ -10,17 +10,50 @@ use caper::renderer::{ RenderItem, Transform };
 use noise::{ perlin2, Seed };
 
 fn main() {
+
+
+
+    // create a vector of render items
+    let render_items = vec![
+        RenderItem {
+            vertices: gen_perlin_mesh(),
+            shader_index: 2,
+            instance_transforms: vec![
+                Transform {
+                    pos: (0.0, 0.0, 0.0),
+                    rot: (0f32, 0f32, 0f32, 1f32),
+                    scale: (1f32, 1f32, 1f32)
+                }
+            ]
+        }
+    ];
+
+    game_loop! {
+        // pass the items to be rendered
+        render_items,
+        // define a block for update
+        { 
+            // update some items
+            //let update_time = clock_ticks::precise_time_s();
+        }
+    }
+}
+
+fn gen_perlin_mesh() -> Vec<Vertex> {
     // generate the instance positions 
     let map_size = 50f32;
     let mut vertices = Vec::new();
 
     for i in 0 .. 2500 {
         let pos = ((i as f32 % map_size), ((i / map_size as i32)) as f32);
+
+        // get all four possible heights for the chunk
         let size_00 = perlin2(&Seed::new(0), &[pos.0 / 10f32, pos.1 / 10f32]).abs() * 8f32;
         let size_10 = perlin2(&Seed::new(0), &[(pos.0 + 1f32) / 10f32, pos.1 / 10f32]).abs() * 8f32;
         let size_01 = perlin2(&Seed::new(0), &[pos.0 / 10f32, (pos.1 + 1f32) / 10f32]).abs() * 8f32;
         let size_11 = perlin2(&Seed::new(0), &[(pos.0 + 1f32) / 10f32, (pos.1 + 1f32) / 10f32]).abs() * 8f32;
 
+        // create the two tris for this chunk
         vertices.push(Vertex {
             position: [pos.0 + 1f32, size_10, pos.1],
             normal: [0f32, 0f32, 0f32],
@@ -53,29 +86,5 @@ fn main() {
         });
     }
 
-
-    // create a vector of render items
-    let render_items = vec![
-        RenderItem {
-            vertices: vertices,
-            shader_index: 2,
-            instance_transforms: vec![
-                Transform {
-                    pos: (0.0, 0.0, 0.0),
-                    rot: (0f32, 0f32, 0f32, 1f32),
-                    scale: (1f32, 1f32, 1f32)
-                }
-            ]
-        }
-    ];
-
-    game_loop! {
-        // pass the items to be rendered
-        render_items,
-        // define a block for update
-        { 
-            // update some items
-            //let update_time = clock_ticks::precise_time_s();
-        }
-    }
+    vertices
 }
