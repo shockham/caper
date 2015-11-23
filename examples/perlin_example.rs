676787853ec6679d8f48bb15eb1efdc9989080f3
shockham <samuel.hockham@gmail.com>
2015-11-23 20:15:53 +0000

    refactored out the game_loop macro for the perlin_example for more granular control

--- examples/perlin_example.rs
+++ examples/perlin_example.rs
@@ -4,15 +4,27 @@ extern crate clock_ticks;
 extern crate caper;
 
 extern crate noise;
-
+use std::thread;
+use std::time::Duration;
+use caper::input::{ Input, Key };
+use caper::shader::Shaders;
 use caper::utils::Vertex;
-use caper::renderer::{ RenderItem, Transform };
+use caper::renderer::{ RenderItem, Transform, Renderer, CamState, FIXED_TIME_STAMP};
 use noise::{ perlin2, Seed };
 
 fn main() {
+    let mut input = Input::new();
+    let renderer = Renderer::new();
+    let shaders = Shaders::new(&renderer.display);
 
+    renderer.setup();
 
-
+    //cam state
+    let mut cam_state = CamState {
+        cam_pos: (0.0f32, 0.0, 0.0),
+        cam_rot: (0.0f32, 0.0, 0.0)
+    };
+    
     // create a vector of render items
     let render_items = vec![
         RenderItem {
@@ -28,14 +40,35 @@ fn main() {
         }
     ];
 
-    game_loop! {
-        // pass the items to be rendered
-        render_items,
-        // define a block for update
-        { 
-            // update some items
-            //let update_time = clock_ticks::precise_time_s();
+    // the main loop
+    let mut accumulator = 0;
+    let mut previous_clock = clock_ticks::precise_time_ns();
+    loop {
+        renderer.draw(cam_state, &render_items, &shaders);
+
+        let now = clock_ticks::precise_time_ns();
+        accumulator += now - previous_clock;
+        previous_clock = now;
+
+        while accumulator >= FIXED_TIME_STAMP {
+            accumulator -= FIXED_TIME_STAMP;
+
+            // updating and handling the inputs
+            input.update_inputs(&renderer.display);
+            input.handle_fp_inputs(&mut cam_state);
+
+            { 
+                // update some items
+                //let update_time = clock_ticks::precise_time_s();
+            }
         }
+
+        //quit
+        if input.keys_down.contains(&Key::Escape) { break; }
+
+        let sleep_duration =
+            Duration::from_millis(((FIXED_TIME_STAMP - accumulator) / 1000000) as u64);
+        thread::sleep(sleep_duration);
     }
 }
 
