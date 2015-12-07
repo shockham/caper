a978d082b359211b704f5ddb5a9d748252db41ee
shockham <samuel.hockham@gmail.com>
2015-12-07 20:23:37 +0000

    playing around with making it look like there is water in the perlin_example

--- src/shader/height.rs
+++ src/shader/height.rs
@@ -53,8 +53,10 @@ pub mod gl330 {
 
             float col_val = normalize(g_pos).y;
             vec3 base_color = vec3(col_val);
-
             base_color += dist; 
+
+            base_color.r *= step(0.05, col_val);
+
 
             vec3 color = base_color * ((0.2 * lum) + (0.8 * dist));
             frag_output = vec4(color, 1.0);
