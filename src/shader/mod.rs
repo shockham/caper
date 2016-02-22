39c85af7db6205091aab7160e63739d48a84d5b0
shockham <samuel.hockham@gmail.com>
2016-02-22 10:53:14 +0000

    tiny shader refactor

--- src/shader/mod.rs
+++ src/shader/mod.rs
@@ -14,7 +14,7 @@ impl Shaders {
     /// Creates a new instance of Shaders
     pub fn new(display: &Display) -> Shaders {
         // the shader programs
-        let program_dist = match program!(display,
+        let program_dist = program!(display,
                                     330 => {
                                         vertex: default::gl330::VERT,
                                         fragment: dist::gl330::FRAG,
@@ -25,59 +25,47 @@ impl Shaders {
                                     110 => {
                                         vertex: default::gl110::VERT,
                                         fragment: dist::gl110::FRAG
-                                    }) {
-            Ok(p) => p,
-            Err(e) => panic!("glsl error: {}", e), 
-        };
+                                    }).unwrap();
 
-        let program_pbr = match program!(display,
-                                    330 => {
-                                        vertex: default::gl330::VERT,
-                                        fragment: pbr::gl330::FRAG,
-                                        geometry:default::gl330::GEOM,
-                                        tessellation_control: default::gl330::TESS_CONTROL,
-                                        tessellation_evaluation: default::gl330::TESS_EVAL
-                                    },
-                                    110 => {
-                                        vertex: default::gl110::VERT,
-                                        fragment: pbr::gl110::FRAG
-                                    }) {
-            Ok(p) => p,
-            Err(e) => panic!("glsl error: {}", e), 
-        };
-        
-        let program_height = match program!(display,
-                                    330 => {
-                                        vertex: default::gl330::VERT,
-                                        fragment: height::gl330::FRAG,
-                                        geometry: default::gl330::GEOM,
-                                        tessellation_control: default::gl330::TESS_CONTROL,
-                                        tessellation_evaluation: default::gl330::TESS_EVAL
-                                    },
-                                    110 => {
-                                        vertex: default::gl110::VERT,
-                                        fragment: height::gl110::FRAG
-                                    }) {
-            Ok(p) => p,
-            Err(e) => panic!("glsl error: {}", e), 
-        };
-        
-        let program_height_tess = match program!(display,
-                                    330 => {
-                                        vertex: default::gl330::VERT,
-                                        fragment: height_tess::gl330::FRAG,
-                                        geometry: default::gl330::GEOM,
-                                        tessellation_control: height_tess::gl330::TESS_CONTROL,
-                                        tessellation_evaluation: height_tess::gl330::TESS_EVAL
-                                    },
-                                    110 => {
-                                        vertex: default::gl110::VERT,
-                                        fragment: height_tess::gl110::FRAG
-                                    }) {
-            Ok(p) => p,
-            Err(e) => panic!("glsl error: {}", e), 
-        };
-       
+        let program_pbr = program!(display,
+                                   330 => {
+                                       vertex: default::gl330::VERT,
+                                       fragment: pbr::gl330::FRAG,
+                                       geometry:default::gl330::GEOM,
+                                       tessellation_control: default::gl330::TESS_CONTROL,
+                                       tessellation_evaluation: default::gl330::TESS_EVAL
+                                   },
+                                   110 => {
+                                       vertex: default::gl110::VERT,
+                                       fragment: pbr::gl110::FRAG
+                                   }).unwrap();
+
+        let program_height = program!(display,
+                                      330 => {
+                                          vertex: default::gl330::VERT,
+                                          fragment: height::gl330::FRAG,
+                                          geometry: default::gl330::GEOM,
+                                          tessellation_control: default::gl330::TESS_CONTROL,
+                                          tessellation_evaluation: default::gl330::TESS_EVAL
+                                      },
+                                      110 => {
+                                          vertex: default::gl110::VERT,
+                                          fragment: height::gl110::FRAG
+                                      }).unwrap();
+
+        let program_height_tess = program!(display,
+                                           330 => {
+                                               vertex: default::gl330::VERT,
+                                               fragment: height_tess::gl330::FRAG,
+                                               geometry: default::gl330::GEOM,
+                                               tessellation_control: height_tess::gl330::TESS_CONTROL,
+                                               tessellation_evaluation: height_tess::gl330::TESS_EVAL
+                                           },
+                                           110 => {
+                                               vertex: default::gl110::VERT,
+                                               fragment: height_tess::gl110::FRAG
+                                           }).unwrap();
+
         Shaders {
             shaders: vec![program_dist, program_pbr, program_height, program_height_tess]
         }
