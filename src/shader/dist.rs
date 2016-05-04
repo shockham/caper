c5ed06fcd65b2b4c693ac1268fc85d3dad8a460e
shockham <samuel.hockham@gmail.com>
2016-05-04 10:54:16 +0100

    removed legacy dist shader

--- src/shader/dist.rs
+++ src/shader/dist.rs
@@ -22,26 +22,3 @@ pub mod gl330 {
         }
     ";
 }
-
-pub mod gl110 { 
-    // fragment shader
-    pub const FRAG: &'static str =
-        "
-        #version 110
-
-        const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
-
-        varying vec3 v_normal;
-        varying vec3 v_pos;
-
-        void main() {
-            float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
-            float dist = max(dot(normalize(v_pos), normalize(LIGHT)), 0.0);
-
-            vec3 base_color = vec3(1.0, 1.0, 1.0);
-
-            vec3 color = base_color * (0.3 + (0.2 * lum) + (0.5 * dist));
-            gl_FragColor = vec4(color, 1.0);
-        }
-    ";
-}
