73c9c897ad4ebfc1dfb12be24f979e9935d9e7b5
shockham <samuel.hockham@gmail.com>
2017-04-03 17:19:11 +0100

    Removed old game_loop examples from docs and README

--- src/lib.rs
+++ src/lib.rs
@@ -5,61 +5,6 @@ Small game framework.
 [Example](https://github.com/shockham/caper/blob/master/examples/simple.rs) of a basis for a game:
 
 ```
-#[macro_use]
-extern crate caper;
-
-use caper::types::{ RenderItem, Transform, PhysicsType, MaterialBuilder };
-use caper::mesh::gen_cube;
-
-fn main() {
-    // define some items to be rendered
-    let mut render_items = vec![
-        RenderItem {
-            vertices: gen_cube(),
-            material: MaterialBuilder::default().build().unwrap(),
-            instance_transforms: vec![
-                Transform {
-                    active: true,
-                    pos: (-0.5, 0.0, -5.0),
-                    rot: (0f32, 0f32, 0f32, 1f32),
-                    scale: (1f32, 1f32, 1f32),
-                }
-            ],
-            active: true,
-            physics_type: PhysicsType::None,
-        },
-    ];
-
-    // define a vector for potential text items
-    let text_items = Vec::new();
-
-    game_loop! {
-        // following are identities for access to the frameworks systems
-        Input => input,
-        Renderer => renderer,
-        CamState => cam_state,
-        RenderItems => render_items,
-        TextItems => text_items,
-        // define a block for start
-        start => {
-            println!("{:?}", cam_state.cam_pos);
-        },
-        // define block for update
-        update => {
-            input.handle_fp_inputs(&mut cam_state);
-        },
-        // block for ui rendering
-        ui => {
-
-        }
-    }
-}
-```
-
-Another [example](https://github.com/shockham/caper/blob/master/examples/game.rs) using the Game struct:
-
-
-```
 extern crate caper;
 
 use caper::types::{ RenderItem, Transform, PhysicsType, MaterialBuilder };
