156d4421bda33ec7c200073c4cc89c51f90947cc
shockham <samuel.hockham@gmail.com>
2017-04-10 21:30:39 +0100

    Added initial fairly crude blurring and bokeh to default post processing shader

--- src/shader/post.rs
+++ src/shader/post.rs
@@ -35,16 +35,33 @@ pub mod gl330 {
 
         out vec4 frag_output;
 
+        float offset[3] = float[]( 0.0, 1.5, 3.0 );
+        float weight[3] = float[]( 0.1, 0.15, 0.03 );
+
         void main() {
             vec4 color = texture(tex, v_tex_coords);
             float depth = texture(depth_buf, v_tex_coords).r;
 
             // pseudo chromatic aberration
-            vec2 edge_offset = vec2(cos(v_tex_coords.x * M_PI), cos(v_tex_coords.y * M_PI)) * chrom_offset;
-            float chrom_r = texture(tex, v_tex_coords + edge_offset).r;
-            color.r = mix(color.r, chrom_r, chrom_amt);
+            if (chrom_amt > 0.0) {
+                vec2 edge_offset = vec2(cos(v_tex_coords.x * M_PI), cos(v_tex_coords.y * M_PI)) * chrom_offset;
+                float chrom_r = texture(tex, v_tex_coords + edge_offset).r;
+                color.r = mix(color.r, chrom_r, chrom_amt);
+            }
 
-            frag_output = color;
+            // blur
+            vec3 tc = color.rgb;
+            for (int i=1; i<3; i++) {
+                tc += texture(tex, v_tex_coords + vec2(0.0, offset[i]) / resolution.y).rgb * weight[i];
+                tc += texture(tex, v_tex_coords - vec2(0.0, offset[i]) / resolution.y).rgb * weight[i];
+                tc += texture(tex, v_tex_coords + vec2(offset[i], 0.0) / resolution.x).rgb * weight[i];
+                tc += texture(tex, v_tex_coords - vec2(offset[i], 0.0) / resolution.x).rgb * weight[i];
+                tc += texture(tex, v_tex_coords + vec2(offset[i] / resolution.x, offset[i] / resolution.y)).rgb * weight[i];
+                tc += texture(tex, v_tex_coords - vec2(offset[i] / resolution.x, offset[i] / resolution.y)).rgb * weight[i];
+            }
+
+            // mix with depth buffer for bokeh
+            frag_output = mix(color, vec4(tc, 1.0), sin(depth * M_PI / 2.0));
         }
     ";
 }
