03e76bf1cf549edc4290514b2d18657fc8b994a8
shockham <samuel.hockham@gmail.com>
2017-04-27 10:50:07 +0100

    Added sphere example showing different segmenting

---
+++ examples/spheres.rs
@@ -0,0 +1,58 @@
+extern crate caper;
+
+use caper::types::{ RenderItemBuilder, TransformBuilder };
+use caper::game::Game;
+use caper::mesh::{ gen_sphere, gen_sphere_segments };
+use caper::imgui::Ui;
+use caper::input::Key;
+
+fn main() {
+    // crate an instance of the game struct
+    let mut game = Game::new();
+
+    // define some items to be rendered
+    game.add_render_item(
+        RenderItemBuilder::default()
+            .vertices(gen_sphere())
+            .instance_transforms(vec![
+                TransformBuilder::default()
+                    .pos((0.0, 0.0, -5.0))
+                    .build()
+                    .unwrap()
+            ])
+            .build()
+            .unwrap());
+    game.add_render_item(
+        RenderItemBuilder::default()
+            .vertices(gen_sphere_segments(10f32, 5f32))
+            .instance_transforms(vec![
+                TransformBuilder::default()
+                    .pos((-5.0, 0.0, -5.0))
+                    .build()
+                    .unwrap()
+            ])
+            .build()
+            .unwrap());
+    game.add_render_item(
+        RenderItemBuilder::default()
+            .vertices(gen_sphere_segments(5f32, 10f32))
+            .instance_transforms(vec![
+                TransformBuilder::default()
+                    .pos((5.0, 0.0, -5.0))
+                    .build()
+                    .unwrap()
+            ])
+            .build()
+            .unwrap());
+
+    loop {
+        // run the engine update
+        game.update(|_:&Ui|{ });
+
+        // update the first person inputs
+        game.input.handle_fp_inputs(&mut game.cam_state);
+
+        // quit
+        if game.input.keys_down.contains(&Key::Escape) { break; }
+    }
+}
