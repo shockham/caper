a7a1ff3115e207e2fe07d885d0868a726ac32526
shockham <samuel.hockham@gmail.com>
2017-05-25 10:13:48 +0100

    Remove row-major frustum plane extraction

--- src/renderer.rs
+++ src/renderer.rs
@@ -170,39 +170,6 @@ impl Renderer {
                      matrix[3][2] - matrix[2][2],
                      matrix[3][3] - matrix[2][3]));
 
-        /*// row-major
-        // Left clipping plane
-        planes.push((matrix[0][3] + matrix[0][0],
-        matrix[1][3] + matrix[1][0],
-        matrix[2][3] + matrix[2][0],
-        matrix[3][3] + matrix[3][0]));
-        // Right clipping plane
-        planes.push((matrix[0][3] - matrix[0][0],
-        matrix[1][3] - matrix[1][0],
-        matrix[2][3] - matrix[2][0],
-        matrix[3][3] - matrix[3][0]));
-        // Top clipping plane
-        planes.push((matrix[0][3] - matrix[0][1],
-        matrix[1][3] - matrix[1][1],
-        matrix[2][3] - matrix[2][1],
-        matrix[3][3] - matrix[3][1]));
-        // Bottom clipping plane
-        planes.push((matrix[0][3] + matrix[0][1],
-        matrix[1][3] + matrix[1][1],
-        matrix[2][3] + matrix[2][1],
-        matrix[3][3] + matrix[3][1]));
-        // Near clipping plane
-        planes.push((matrix[0][2],
-        matrix[1][2],
-        matrix[2][2],
-        matrix[3][2]));
-        // Far clipping plane
-        planes.push((matrix[0][3] - matrix[0][2],
-        matrix[1][3] - matrix[1][2],
-        matrix[2][3] - matrix[2][2],
-        matrix[3][3] - matrix[3][2]));*/
-
-
         planes
     }
 
