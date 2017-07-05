3a02b60555c8f83d3712c557825229a20ec00622
shockham <samuel.hockham@gmail.com>
2017-07-05 14:58:49 +0100

    Update genmesh and obj deps and fix fallout

--- src/utils.rs
+++ src/utils.rs
@@ -16,13 +16,13 @@ pub fn load_wavefront( data: &[u8]) -> Vec<Vertex> {
 
     let mut vertex_data = Vec::new();
 
-    for shape in data.object_iter().next().unwrap().group_iter().flat_map(|g| g.indices().iter()) {
+    for shape in data.objects.iter().next().unwrap().groups.iter().flat_map(|g| g.indices.iter()) {
         match shape {
             &genmesh::Polygon::PolyTri(genmesh::Triangle { x: v1, y: v2, z: v3 }) => {
                 for v in [v1, v2, v3].iter() {
-                    let position = data.position()[v.0];
-                    let texture = v.1.map(|index| data.texture()[index]);
-                    let normal = v.2.map(|index| data.normal()[index]);
+                    let position = data.position[v.0];
+                    let texture = v.1.map(|index| data.texture[index]);
+                    let normal = v.2.map(|index| data.normal[index]);
 
                     let texture = texture.unwrap_or([0.0, 0.0]);
                     let normal = normal.unwrap_or([0.0, 0.0, 0.0]);
